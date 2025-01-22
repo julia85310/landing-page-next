5ebe2188a0e301995ad97ca799d4aeefbb9076c1
julia85310 <148855799+julia85310@users.noreply.github.com>
2025-01-22 11:11:46 +0100

    terminado

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,41 @@
-
-
 export default function Home() {
   return (
     <div>
       <header>
-        <img src="/Logo-asisa-removebg-preview.png"></img>
-        <nav></nav>
+        <img src="/Logo-asisa-removebg-preview.png" id="icono"></img>
+        <nav>
+          <a href="#">Chat Médico</a>
+          <a href="#">Videoconsulta</a>
+          <a href="#">Apoyo Psicoemocional</a>
+          <a href="#">Telerrehabilitación</a>
+        </nav>
       </header>
       <main>
-
+        <div id="imagen1">
+          <img src="/Cuida-tu-salud.jpg"></img>
+          <p>Tu salud es lo más importante. Con Asisa, disfrutas de un acceso rápido y eficiente a los mejores servicios médicos. Protege tu bienestar con nosotros.</p>
+        </div>
+        <div id="imagen2">
+          <img src="/medicos-especialistas.jpg"></img>
+          <p>Cuidamos de ti y de los tuyos con un seguro de salud adaptado a tus necesidades. Asisa te ofrece cobertura completa y atención personalizada en cada momento.</p>
+        </div>
+        <div id="imagen3">
+          <img src="/medicos.jpg"></img>
+          <p>Confía en la experiencia de Asisa para mantener tu salud en las mejores manos. Con nuestros planes de salud, podrás vivir tranquilo sabiendo que estás cubierto.</p>
+        </div>
       </main>
       <footer>
-
+        <ul>
+          <li>
+              <a href="#">Nosotros</a> 
+          </li>
+          <li>
+            <a href="#">Red de Centros Médicos</a> 
+          </li>
+          <li>
+            <a href="#">Trabaja con nosotros</a>
+          </li>
+        </ul>
       </footer>
     </div>
   );
